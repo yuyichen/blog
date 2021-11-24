d20e715e8cd957675d0312900a3e591308a5f5a1
李力 <lili@shulie.io>
2021-11-24 17:22:01 +0800

    revert margin

--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -3,7 +3,7 @@ import donateImg from "@/assets/imgs/donate.jpeg";
 export default () => {
   return (
     <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
-      <div className="w-full bg-white shadow flex flex-col my-4 md:my-0 p-6">
+      <div className="w-full bg-white shadow flex flex-col my-4 p-6">
         <p className="text-xl font-semibold pb-5">关于我</p>
         <p className="pb-2 text-gray-600">
           资深前端开发工程师，坐标杭州，我会持续分享前端工作以及个人建站方面的技术，如果觉得我的文章对你有帮助，欢迎赞赏。
