eedd6764d3c315c41d4b30ab74f132b95d7d8627
李力 <lili@shulie.io>
2021-12-03 10:48:42 +0800

    style: loading组件样式调整

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -5,11 +5,18 @@ import classnames from "classnames";
 export default (props) => {
   const { children, loading = true, className, ...rest } = props;
   return (
-    <div className={classnames("relative min-h-30 w-full", className)} {...rest}>
+    <div
+      className={classnames("relative min-h-30 w-full", className)}
+      {...rest}
+    >
       {children}
       {loading && (
-        <div className="absolute top-0 right-0 bottom-0 left-0 bg-white bg-opacity-80 backdrop-blur-sm z-20 flex items-center justify-center">
-          <img src={loadingImg} style={{ width: 400 }} />
+        <div className="absolute top-0 right-0 bottom-0 left-0 bg-white z-20">
+          <img
+            src={loadingImg}
+            style={{ width: 400 }}
+            className="block mx-auto mt-20"
+          />
         </div>
       )}
     </div>
