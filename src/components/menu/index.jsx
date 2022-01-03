64b40401564304c6176b314656512acd6b500a65
lili <lil@shinemo.com>
2022-01-03 10:35:32 +0800

    fix: bug

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,10 +1,9 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { motion, useCycle } from "framer-motion";
 import useDimensions from "./useDimensions";
 import MenuToggle from "./toggle";
 import Navigation from "./list";
 import classNames from "classnames";
-import { useEffect } from "react/cjs/react.development";
 
 const sidebar = {
   open: (height = 1000) => ({
