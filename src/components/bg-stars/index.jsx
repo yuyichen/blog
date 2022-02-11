1598e64b3ff89fdda653e65303e041d2d7e92de6
李力 <lili@shulie.io>
2022-02-11 15:08:27 +0800

    chore: github deploy action

--- src/components/bg-stars/index.jsx
+++ src/components/bg-stars/index.jsx
@@ -1,9 +1,11 @@
-import './index.less';
+import "./index.less";
 
 export default () => {
-  return <div id="stars-box">
-    <div id="stars"></div>
-    <div id="stars2"></div>
-    <div id="stars3"></div>
-  </div>
-}
+  return (
+    <div id="stars-box">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+};
