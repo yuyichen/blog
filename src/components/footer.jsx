dd4e71b85eb3006e7a7f5cdcd216460f7253cea8
李力 <lili@shulie.io>
2022-02-17 11:52:48 +0800

    feat: 页脚调整

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,6 +12,22 @@ export default () => {
             <span className="iconfont icon-github" />
           </a>
           <a
+            href="https://weibo.com/u/2617682101"
+            target="_blank"
+            className="px-3 hover:text-blue-500 transition"
+            title="weibo"
+          >
+            <span className="iconfont icon-weibo-fill" />
+          </a>
+          <a
+            href="https://www.zhihu.com/people/li-li-28-1-49"
+            target="_blank"
+            className="px-3 hover:text-blue-500 transition"
+            title="知乎"
+          >
+            <span className="iconfont icon-29" />
+          </a>
+          <a
             href="https://huaban.com/jaawfnb7wg/"
             target="_blank"
             className="px-3 hover:text-blue-500 transition"
@@ -21,7 +37,7 @@ export default () => {
           </a>
         </div>
         <div className="pb-2">浙ICP备2021034868号-1</div>
-        <div className="pb-3">&copy; 2021 yuyichen.space</div>
+        <div className="pb-3">&copy; 2021-2024 yuyichen.space</div>
       </div>
     </footer>
   );
